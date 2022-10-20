716a8aebce14307bdf1c9e23f7cfbe52274b5361
Sean Joseph <sean_joseph@protonmail.com>
2022-10-19 20:44:49 -0700

    deleted design pictures and npm package

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const undreadNotifications = document.querySelectorAll(".unread");
+const button = document.querySelector("button");
+const count = document.getElementById("notification-count");
 
+// on page load
 addCircles();
 
+button.addEventListener("click", () => {
+    //remove custom styling
+    undreadNotifications.forEach(element => {
+        element.classList.remove("unread");
+    })
+    // change Notification count
+    count.textContent = "0";
+
+})
 function addCircles(){
 undreadNotifications.forEach(elem => {
     const newSpan = document.createElement("span");
